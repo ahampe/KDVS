c8cf40adedc6fa7a229f0cc9655711ab1ac9b775
Alex Hampe <alex.hampe@powerschool.com>
2018-12-11 20:41:21 -0800

    changed theme and play button

--- app/src/main/java/fho/kdvs/MainActivity.kt
+++ app/src/main/java/fho/kdvs/MainActivity.kt
@@ -3,94 +3,71 @@ package fho.kdvs
 import android.media.AudioAttributes
 import android.media.MediaPlayer
 import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
-import android.widget.Toast
+import android.view.View
+import android.widget.ToggleButton
 import kotlinx.android.synthetic.main.activity_main.*
-import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
-    private var mPlayer: MediaPlayer? = null
+    private val mPlayer = MediaPlayer()
+
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.navigation_home -> {
+                // Home page
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_dashboard -> {
+                // Programming grid
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_notifications -> {
+                // Settings / other stuff
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        playButton.setOnClickListener { playButton ->
-            // Disable the play button
-            playButton.isEnabled = false
-            pauseButton.isEnabled = true
+        // Prepare media player
+        mPlayer.apply {
+            setAudioAttributes(audioAttrs)
+            setDataSource(streamUrl)
 
-            // The audio url to play
-            val audioUrl = "http://archives.kdvs.org:8000/kdvs128mp3"
-
-            // Initialize a new media player instance
-            mPlayer = MediaPlayer()
-
-            // Set audio attributes
-            val audioAttrs = AudioAttributes.Builder()
-                .setUsage(AudioAttributes.USAGE_MEDIA)
-                .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
-                .build()
-
-            // Set the media player audio stream type
-            mPlayer?.setAudioAttributes(audioAttrs)
-            //Try to play music/audio from url
-            try {
-                // Set the audio data source
-                mPlayer?.setDataSource(audioUrl)
-
-                // Prepare the media player
-                mPlayer?.prepare()
-
-                // Start playing audio from http url
-                mPlayer?.start()
-
-                // Inform user for audio streaming
-                Toast.makeText(this, "Playing", Toast.LENGTH_SHORT).show()
-            } catch (e: IOException) {
-                // Catch the exception
-                e.printStackTrace()
-            } catch (e: IllegalArgumentException) {
-                e.printStackTrace()
-            } catch (e: SecurityException) {
-                e.printStackTrace()
-            } catch (e: IllegalStateException) {
-                e.printStackTrace()
+            // Prepare the audio stream in the background, and enable the play button only when ready
+            prepareAsync()
+            setOnPreparedListener {
+                playButton.isEnabled = true
             }
         }
 
-        pauseButton.setOnClickListener { _ ->
-            mPlayer?.pause()
-            playButton.isEnabled = true
-            pauseButton.isEnabled = false
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+    }
+
+    fun onPlayPause(v: View) {
+        val button = v as? ToggleButton ?: return
+        try {
+            if (!mPlayer.isPlaying) {
+                mPlayer.start()
+            } else {
+                mPlayer.pause()
+            }
+        } catch (e: Exception) {
+            button.isChecked = !button.isChecked
+            e.printStackTrace()
         }
     }
-}
 
-//class MainActivity : AppCompatActivity() {
-//
-//    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-//        when (item.itemId) {
-//            R.id.navigation_home -> {
-//                message.setText(R.string.title_home)
-//                return@OnNavigationItemSelectedListener true
-//            }
-//            R.id.navigation_dashboard -> {
-//                message.setText(R.string.title_dashboard)
-//                return@OnNavigationItemSelectedListener true
-//            }
-//            R.id.navigation_notifications -> {
-//                message.setText(R.string.title_notifications)
-//                return@OnNavigationItemSelectedListener true
-//            }
-//        }
-//        false
-//    }
-//
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_main)
-//
-//        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
-//    }
-//}
+    companion object {
+        private val audioAttrs = AudioAttributes.Builder()
+            .setUsage(AudioAttributes.USAGE_MEDIA)
+            .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+            .build()
+        private const val streamUrl = "http://archives.kdvs.org:8000/kdvs128mp3"
+    }
+}
