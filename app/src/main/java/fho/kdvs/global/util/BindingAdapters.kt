319497fc35697fa9477ff334813766035ce18c3a
scjohnson16 <scjohnso@ucdavis.edu>
2019-03-13 23:26:31 -0700

    html will now render in descriptions correctly, with clickable links

--- app/src/main/java/fho/kdvs/global/util/BindingAdapters.kt
+++ app/src/main/java/fho/kdvs/global/util/BindingAdapters.kt
@@ -1,5 +1,6 @@
 package fho.kdvs.global.util
 
+import android.text.Html
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
@@ -28,4 +29,14 @@ fun loadImageWithGlide(view: ImageView, imageHref: String?) {
 @BindingAdapter("localDate", "dateFormatter")
 fun safeFormatDate(view: TextView, date: LocalDate?, dateFormatter: DateTimeFormatter) {
     view.text = date?.let { dateFormatter.format(it) } ?: ""
+}
+
+@BindingAdapter("desc")
+fun formatDescHtml(view: TextView, desc: String?){
+    val descWithBreaks = desc?.replace("\n", "<br/><br/>")
+    if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+        view.text = Html.fromHtml(descWithBreaks ?: "", Html.FROM_HTML_MODE_LEGACY)
+    } else {
+        view.text = (Html.fromHtml(descWithBreaks ?: ""))
+    }
 }
